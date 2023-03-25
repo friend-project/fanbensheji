5394ebe38c7f006cb8967de072ed4d1c637aa498
zZ <z@zZ.local>
2023-03-25 23:39:29 +0800

    feat: add company

--- projects/api/server/controller/upload.js
+++ projects/api/server/controller/upload.js
@@ -1,49 +1,26 @@
 const fs = require('fs');
 const path = require('path');
-const jimp = require('jimp');
+const sharp = require('sharp');
 
 const upload = async (ctx) => {
   try {
     const { file } = ctx.request.files;
     const { fileType } = ctx.request.body;
+    const fileName = `${fileType}_${Date.now()}_.${file.name.split('.').pop().toLowerCase()}`;
+    const filePath = path.join(__dirname, '../../upload', fileName);
+    const watermarkPath = path.join(__dirname, './watermark/watermark.png');
 
-    const fileExt = file.name.split('.').pop().toLowerCase();
-    const fileName = `${fileType}_${Date.now()}.jpg`;
-    const filePath = `${path.join(__dirname, '../../upload')}/${fileName}`;
-    const upStream = fs.createWriteStream(filePath);
-
-    const reader = fs.createReadStream(file.path);
-    reader.pipe(upStream);
-
-    upStream.on('finish', () => {
-      fs.chmod(filePath, 0o777, (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
-    });
-
-    let rst = fileName;
-
+    // 上传并添加水印
     if (fileType === 'project') {
-      // 添加水印
-      const watermarkPath = `${path.join(__dirname, './watermark.png')}`;
-      const image = await jimp.read(filePath);
-      const watermark = await jimp.read(watermarkPath);
-
-      const watermarkWidth = 326.75;
-      const watermarkHeight = 26.75;
-      const watermarkX = Math.floor((image.bitmap.width - watermarkWidth) / 2);
-      const watermarkY = Math.floor((image.bitmap.height - watermarkHeight) / 2);
-
-      image.composite(watermark, watermarkX, watermarkY, {
-        mode: jimp.BLEND_SOURCE_OVER,
-        opacityDest: 1,
-        opacitySource: 0.5
-      });
-
-      await image.writeAsync(filePath);
+      await sharp(file.path)
+        .composite([{ input: watermarkPath, top: 100, left: 100 }])
+        .composite([{ input: watermarkPath, gravity: 'center' }]) // 添加水印
+        .toFile(filePath); // 输出到文件
+    } else {
+      await sharp(file.path).toFile(filePath);
     }
+
+    const rst = fileName;
 
     ctx.body = {
       code: rst ? 0 : 1,
